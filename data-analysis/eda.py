657498fd34f89822a609f487c74ac72b650af17b
ravi19 <ravichandravedula@gmail.com>
2023-06-23 16:49:17 +0530

    from local repo to remote repo push

---
+++ data-analysis/eda.py
@@ -0,0 +1,16 @@
+"""
+Exploratory Data Analysis With Python for the Tide Data of Indian Tide
+Gauge Network.  
+Date:23 June 2023
+Authror:Ravi Chandra
+
+"""
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Import Input Data
+
+data = pd.read_csv()
